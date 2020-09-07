8beefa5c7b246f30712c1982fddffbec3077d895
Romanos Trechlis <r.trechlis@gmail.com>
2020-09-07 10:48:56 +0300

    add dl command

--- cmd/dl/dl.go
+++ cmd/dl/dl.go
@@ -1,30 +1,33 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 
-	dl "github.com/RomanosTrechlis/go-dl"
+	"github.com/RomanosTrechlis/go-dl"
 )
+
 func main() {
-	l := len(os.Args)
-	d := flag.String("d", "./", "directory to download")
-	if l < 2 {
+	length := len(os.Args)
+	if length < 2 {
 		_, _ = fmt.Fprintf(os.Stderr, "Usage: %s <url> <url> ...", os.Args[0])
 		return
 	}
 
-
-	for i:=1; i < l; i++ {
+	for i := 1; i < length; i++ {
 		url := os.Args[i]
 		f := getFilename(url)
 		if f == "" {
 			f = strconv.Itoa(i)
 		}
-		dl.New(url, *d, f)
+		downloader := dl.New(url, "./", f)
+		err := downloader.Download()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "failed to download file %s: %v", url, err)
+			return
+		}
 	}
 }
 
